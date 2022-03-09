a18da8ef245f0fd394b6d7ede8552cbbcd0c07bb
Beatriz Moreli <79378960+Beamoreli@users.noreply.github.com>
2022-03-09 11:24:49 -0300

    Polimorfismo

--- Heranca/Preguica.java
+++ Heranca/Preguica.java
@@ -1,8 +1,13 @@
 package Heranca;
 
-public class Preguica extends Animal{
+public abstract class Preguica extends Animal{
 
 	 String subir;
+	
+	 @Override
+		public void emitirSom(){
+			System.out.println("Som de Animal.");
+		}
 
 	public String getSubir() 
 	{
